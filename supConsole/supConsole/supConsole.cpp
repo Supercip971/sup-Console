a051889ea8057da1bbe6dd7f069d22b83cfa38c2
supercyp <supercyp971@gmail.com>
2020-01-31 07:34:07 -0400

    add the project

---
+++ supConsole/supConsole/supConsole.cpp
@@ -0,0 +1,12 @@
+﻿// supConsole.cpp : définit le point d'entrée de l'application.
+//
+
+#include "supConsole.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
